ef823af3c9385270aa9a25b611a4d54e1b826833
Suzic <suzicletter@msn.com>
2016-01-28 21:46:29 +0800

    任务中界面更新

--- mgservice/InTaskController.m
+++ mgservice/InTaskController.m
@@ -8,14 +8,15 @@
 
 #import "InTaskController.h"
 
-@interface InTaskController ()
+@interface InTaskController () <UITextFieldDelegate>
 
 @property (strong, nonatomic) IBOutlet UIButton *myLocation;
 @property (strong, nonatomic) IBOutlet UIButton *heLocation;
 @property (strong, nonatomic) IBOutlet UIView *chatHistoryView;
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *chatHistoryViewTop;
-@property (strong, nonatomic) IBOutlet NSLayoutConstraint *chatHistoryViewHeight;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *chatHistoryViewBottom;
 @property (strong, nonatomic) IBOutlet UITextField *inputChat;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *showMap;
 
 @property (assign, nonatomic) BOOL showTalk;
 
@@ -29,10 +30,11 @@
 
     self.myLocation.layer.cornerRadius = 15.0f;
     self.heLocation.layer.cornerRadius = 15.0f;
-    //self.navigationItem.hidesBackButton = YES;
+    self.navigationItem.rightBarButtonItem = nil;
+    self.navigationItem.hidesBackButton = YES;
     
     self.chatHistoryViewTop.constant = self.view.frame.size.height - 124;
-    self.chatHistoryViewHeight.constant = self.view.frame.size.height - 64;
+    self.chatHistoryViewBottom.constant = 60 - self.view.frame.size.height;
     self.showTalk = NO;
 }
 
@@ -51,19 +53,39 @@
 {
     if (_showTalk == showTalk)
         return;
-    _showTalk = showTalk;
     
-    CGRect showHistoryRect = CGRectMake(0, self.view.frame.size.height - self.chatHistoryView.frame.size.height, self.chatHistoryView.frame.size.width, self.chatHistoryView.frame.size.height);
-    CGRect hideHistoryRect = CGRectMake(0, self.view.frame.size.height - 60, self.chatHistoryView.frame.size.width, self.chatHistoryView.frame.size.height);
+    _showTalk = showTalk;
+    self.navigationItem.rightBarButtonItem = showTalk ? self.showMap : nil;
+
+    CGRect showHistoryRect = CGRectMake(0, 64, self.view.frame.size.width, self.view.frame.size.height - 64);
+    CGRect hideHistoryRect = CGRectMake(0, self.view.frame.size.height - 60, self.view.frame.size.width, self.view.frame.size.height - 64);
     [self.chatHistoryView setFrame:showTalk ? hideHistoryRect : showHistoryRect];
     [UIView animateWithDuration:0.5f animations:^{
         [self.chatHistoryView setFrame:showTalk ? showHistoryRect : hideHistoryRect];
+    } completion:^(BOOL finished) {
+        self.chatHistoryViewTop.constant = showTalk ? 0.0f : self.view.frame.size.height - 124;
+        self.chatHistoryViewBottom.constant = showTalk ? 0.0f : 60 - self.view.frame.size.height;
+        if (showTalk == NO)
+            [self.inputChat resignFirstResponder];
     }];
+}
+
+- (IBAction)tapHistory:(id)sender
+{
+    if ([self.inputChat isFirstResponder])
+        [self.inputChat resignFirstResponder];
+    else if (self.showTalk == NO)
+        self.showTalk = YES;
 }
 
 - (IBAction)swithTalk:(id)sender
 {
-    self.showTalk = !self.showTalk;
+    self.showTalk = NO;
+}
+
+- (IBAction)sendChat:(id)sender
+{
+    self.navigationItem.hidesBackButton = !self.navigationItem.hidesBackButton;
 }
 
 /*
@@ -75,5 +97,20 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    if (self.showTalk)
+    {
+        [UIView animateWithDuration:0.5f animations:^{
+            self.chatHistoryViewBottom.constant = 250.0f;
+        }];
+    }
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    self.chatHistoryViewBottom.constant = self.showTalk ? 0.0f : 60 - self.view.frame.size.height;
+}
 
 @end
