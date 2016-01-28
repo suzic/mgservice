4481addf451b2ff89501aa16d8bdaa3b7aad2df2
Suzic <suzicletter@msn.com>
2016-01-28 10:48:04 +0800

    Initial Commit

---
+++ mgservice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mgservice
+//
+//  Created by 苏智 on 16/1/28.
+//  Copyright © 2016年 Suzic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
